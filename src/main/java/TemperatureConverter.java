fd8d6980e9dfeddc79bfd6b51838ee4d4dd30437
riikka <riikaka@metropolia.fi>
2026-03-02 10:49:29 +0200

    Added a main method to TemperatureConverter class

--- src/main/java/TemperatureConverter.java
+++ src/main/java/TemperatureConverter.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class TemperatureConverter {
 
     public double fahrenheitToCelsius(double fahrenheit) {
@@ -15,6 +17,35 @@ public class TemperatureConverter {
     public boolean isExtremeTemperature(double celsius) {
         return (celsius > 50 || celsius < -40) ? true : false;
         //return celsius > 50 || celsius < -40;  // <-- Suggested simplification
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        TemperatureConverter temperatureConverter = new TemperatureConverter();
+
+        System.out.println("Enter Fahrenheits to convert to Celsius: ");
+        double f = scanner.nextDouble();
+        double c = temperatureConverter.fahrenheitToCelsius(f);
+        System.out.println(f + "°F in Celsius: " + c + "°C");
+        if (temperatureConverter.isExtremeTemperature(c)) {
+            System.out.println("It is an extreme temperature.");
+        } else {
+            System.out.println("It is not an extreme temperature.");
+        }
+
+        System.out.println("Enter Celsius to convert to Fahrenheit: ");
+        double c2 = scanner.nextDouble();
+        System.out.println(c2 + "°C in Fahrenheit: " + temperatureConverter.celsiusToFahrenheit(c2) + "°F");
+
+        System.out.println("Enter Kelvin to convert to Celsius: ");
+        double k = scanner.nextDouble();
+        double c3 = temperatureConverter.kelvinToCelsius(k);
+        System.out.println(c2 + "°K in Celsius: " + c3 + "°C");
+        if (temperatureConverter.isExtremeTemperature(c3)) {
+            System.out.println("It is an extreme temperature.");
+        } else {
+            System.out.println("It is not an extreme temperature.");
+        }
     }
 
 
